d6decb41b8cb6f079380e2d8a71d001b9a4d128d
Preom Rahman <preom.m.r@gmail.com>
2015-08-08 07:39:33 -0400

    Move tagging to separate module

--- generate.py
+++ generate.py
@@ -342,44 +342,6 @@ class IterativeBuilder(GenerationComponent):
         """
         pass
 
-class CategoryGenerator(GenerationComponent):
-    def __init__(self, settings):
-        super(CategoryGenerator, self).__init__(settings)
-
-    def get_tags(self, fname=None):
-        """ Get a list of all the tags in the database as a row object. 
-
-            Attributes:
-                fname: Standardized form of the filename stored in the database.
-
-            Returns: 
-                [str]: list of tags.
-
-        """
-        dbManager = DatabaseManager(self.settings)
-        conn, cursor = dbManager.get_conn()
-
-        sqlString = 'Select distinct tag from tags order by tag'
-
-        cursor.execute(sqlString)
-        return cursor.fetchall()
-
-    def get_fnames_from_tag(self, tagName):
-        """ Get filenames with a tag matching tagName. """
-        dbManager = DatabaseManager(self.settings)
-        conn, cursor = dbManager.get_conn()
-
-        sqlString = 'Select tags.filename, title from tags left join meta on tags.filename=meta.filename where tags.tag="{}"'.format(tagName)
-
-        cursor.execute(sqlString)
-
-        return cursor.fetchall()
-
-
-    def tag_page(self, tagName):
-        """ Return the links to posts that use 'tagName' """
-        pass
-
 
 class DatabaseManager(GenerationComponent):
     """ Abstraction for db operations. """
