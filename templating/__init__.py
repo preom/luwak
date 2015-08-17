f63612cfddee8a16aa2b3b29ab924a8fa30c9154
Preom Rahman <preom.m.r@gmail.com>
2015-08-17 07:58:07 -0400

    Comment out Bs4Combinator for  test purposes

--- templating/__init__.py
+++ templating/__init__.py
@@ -79,158 +79,160 @@ class CombinatorBase(object):
             print templateType
             print errorMsg
 
+# class Bs4Combinator(CombinatorBase):
+#     def combine(self, type) :
+#         if type is None or type == 'default':
+#             templateHtml = self.get_template()
+#             templateSoup = BeautifulSoup(templateHtml)
+#             htmlSoup = BeautifulSoup(html)
+
+#             tag = templateSoup.find(class_='luwak-content')
+#             tag.insert(1, htmlSoup)
+#             templateSoup = BeautifulSoup(templateSoup.encode_contents())
+
+#             def cleanup():
+#                 """ Return a cleaned version of a beautifulsoup object.
+
+#                     BeautifulSoup has a bug that prevents finding tags correctly
+#                     after inserting a soup object. Use this function to decod the 
+#                     template soup into text and then parse that string into a 
+#                     BeautifulSoup object again.
+
+#                 """
+#                 return BeautifulSoup(templateSoup.encode_contents())
+
+#             # Formatters for the data to be injected
+#             def date_formatter(metaDate):
+#                 date = datetime.datetime.strptime(metaDate[0], '%Y-%m-%d')
+#                 return date.strftime('%B %d, %Y')
+
+#             def tag_formatter(metaTags):
+#                 tagString = "<span class='tag label label-default'> <span class='glyphicon glyphicon-tag'></span> {}</span>"
+#                 tagHtml = ''.join([tagString.format(tag) for tag in meta['tags']])
+
+#             def prev_formatter(metaPrev):
+#                 tagString = '<a href=\'{}\'><span aria-hidden="true" class="glyphicon glyphicon-menu-left"></span> Prev </a>'
+#                 return tagString.format(metaPrev[0])
+
+#             def next_formatter(metaNext):
+#                 tagString = '<a href=\'{}\'>Next <span aria-hidden="true" class="glyphicon glyphicon-menu-right"></span></a>'
+#                 return tagString.format(metaNext[0])
+
+
+#             metaTags = ['date', 'tags', 'title', 'prev', 'next']
+#             formatters = [date_formatter, tag_formatter, None, prev_formatter, next_formatter]
+#             template_class = ['luwak-date', 'luwak-tags', 'luwak-title', 'luwak-prev', 'luwak-next']
+
+#             assert(len(metaTags) == len(formatters) == len(template_class))
+
+#             for mTag, frmt, cls in zip(metaTags, formatters, template_class):
+#                 if mTag in meta:
+#                     tag = templateSoup.find(class_=cls)
+#                     if frmt:
+#                         tagContent = frmt(meta[mTag])
+#                     else:
+#                         tagContent = meta[mTag][0]
+
+#                     if tagContent:
+#                         soup = BeautifulSoup(tagContent, 'html.parser')
+#                     else:
+#                         continue
+
+#                     try:
+#                         tag.append(soup)
+#                     except:
+#                         print "Error with parsing " + mTag
+
+#                     templateSoup = cleanup()
+
+#             #TODO Fix this arbitrary piece of code
+#             if len(templateSoup) < 3000:
+#                 tag = templateSoup.find(class_='to-top')
+#                 if tag:
+#                     tag['class'].append('hidden')
+
+#             endProduct = templateSoup.prettify()
+
+#             return endProduct
+#         elif type == 'list':
+#             templateHtml = self.get_template('list')
+#             def a_formatter(title, href):
+#                 if not href:
+#                     href = ''
+#                 return "<a href='{1}'>{0}</a>".format(title, href)
+
+#             linksHtml = ''.join([a_formatter(title, href) for title, href in links])
+#             linksSoup = BeautifulSoup(linksHtml)
+#             templateSoup = BeautifulSoup(templateHtml)
+
+#             tag = templateSoup.find(class_='luwak-list')
+#             tag.append(linksSoup)
+
+#             if listTitle:
+#                 tag = templateSoup.find(class_='luwak-list-title')
+#                 tag.string.replace_with(listTitle)
+
+#             return templateSoup.prettify()
+#         elif type == 'index':
+#             def a_formatter(title, href):
+#                 return "<a href='{1}'>{0}</a>".format(title, href)
+
+#             templateHtml = self.get_template('index')
+#             postListHtml = []
+#             for title, href, article in pageInfo['postList']:
+
+#                 aHtml = '<a href=\'{1}\'>{0}</a>'.format(title.title(), href)
+#                 dateHtml = '<span>{}</span>'.format(article['created'].strftime("%b %d, %y"))
+#                 postStr = """
+#                     <div class=\'index-post\'>
+#                         <div class=\'post-title\'>{aHtml}</div>
+#                         <div class=\'post-date\'>{dateHtml}</div>
+#                     </div>
+#                 """
+#                 postListHtml.append(postStr.format(aHtml=aHtml,dateHtml=dateHtml))
+
+#             listSoup = BeautifulSoup('<hr>'.join(postListHtml))    
+#             templateSoup = BeautifulSoup(templateHtml)
+
+#             tag = templateSoup.find(class_='luwak-index')
+#             paginationTag = templateSoup.find(class_='luwak-pagination')
+
+#             if not tag:
+#                 raise ValueError('No luwak-index in index template')
+#             else:
+#                 tag.append(listSoup)
+
+
+#             leftAdjacent = [a_formatter(title, href) for title, href in pageInfo['leftAdjacentPages']]
+#             rightAdjacent = [a_formatter(title, href) for title, href in pageInfo['rightAdjacentPages']]
+
+#             currentpage = a_formatter(pageInfo['currentPage'][0], pageInfo['currentPage'][1])
+
+#             page_list = leftAdjacent + [currentpage] + rightAdjacent
+#             for ind, val in enumerate(page_list):
+#                 if val == currentpage:
+#                     cls = 'active'
+#                 else:
+#                     cls = ''
+
+#                 page_list[ind] = "<li class='{}'>{}</li>".format(cls, val)
+
+
+#             page_list = ['<ul class="pagination pull-right">'] + page_list + ['</ul>']
+
+#             paginationSoup = BeautifulSoup(''.join(page_list))
+
+#             tag = templateSoup.find(class_='luwak-pagination')
+
+#             if paginationTag:
+#                 paginationTag.append(paginationSoup)
+#             else:
+#                 print "WARNING: no page index"
+
+#             return templateSoup.prettify()
 
 class Bs4Combinator(CombinatorBase):
-    def combine(self, type) :
-        if type is None or type == 'default':
-            templateHtml = self.get_template()
-            templateSoup = BeautifulSoup(templateHtml)
-            htmlSoup = BeautifulSoup(html)
-
-            tag = templateSoup.find(class_='luwak-content')
-            tag.insert(1, htmlSoup)
-            templateSoup = BeautifulSoup(templateSoup.encode_contents())
-
-            def cleanup():
-                """ Return a cleaned version of a beautifulsoup object.
-
-                    BeautifulSoup has a bug that prevents finding tags correctly
-                    after inserting a soup object. Use this function to decod the 
-                    template soup into text and then parse that string into a 
-                    BeautifulSoup object again.
-
-                """
-                return BeautifulSoup(templateSoup.encode_contents())
-
-            # Formatters for the data to be injected
-            def date_formatter(metaDate):
-                date = datetime.datetime.strptime(metaDate[0], '%Y-%m-%d')
-                return date.strftime('%B %d, %Y')
-
-            def tag_formatter(metaTags):
-                tagString = "<span class='tag label label-default'> <span class='glyphicon glyphicon-tag'></span> {}</span>"
-                tagHtml = ''.join([tagString.format(tag) for tag in meta['tags']])
-
-            def prev_formatter(metaPrev):
-                tagString = '<a href=\'{}\'><span aria-hidden="true" class="glyphicon glyphicon-menu-left"></span> Prev </a>'
-                return tagString.format(metaPrev[0])
-
-            def next_formatter(metaNext):
-                tagString = '<a href=\'{}\'>Next <span aria-hidden="true" class="glyphicon glyphicon-menu-right"></span></a>'
-                return tagString.format(metaNext[0])
-
-
-            metaTags = ['date', 'tags', 'title', 'prev', 'next']
-            formatters = [date_formatter, tag_formatter, None, prev_formatter, next_formatter]
-            template_class = ['luwak-date', 'luwak-tags', 'luwak-title', 'luwak-prev', 'luwak-next']
-
-            assert(len(metaTags) == len(formatters) == len(template_class))
-
-            for mTag, frmt, cls in zip(metaTags, formatters, template_class):
-                if mTag in meta:
-                    tag = templateSoup.find(class_=cls)
-                    if frmt:
-                        tagContent = frmt(meta[mTag])
-                    else:
-                        tagContent = meta[mTag][0]
-
-                    if tagContent:
-                        soup = BeautifulSoup(tagContent, 'html.parser')
-                    else:
-                        continue
-
-                    try:
-                        tag.append(soup)
-                    except:
-                        print "Error with parsing " + mTag
-
-                    templateSoup = cleanup()
-
-            #TODO Fix this arbitrary piece of code
-            if len(templateSoup) < 3000:
-                tag = templateSoup.find(class_='to-top')
-                if tag:
-                    tag['class'].append('hidden')
-
-            endProduct = templateSoup.prettify()
-
-            return endProduct
-        elif type == 'list':
-            templateHtml = self.get_template('list')
-            def a_formatter(title, href):
-                if not href:
-                    href = ''
-                return "<a href='{1}'>{0}</a>".format(title, href)
-
-            linksHtml = ''.join([a_formatter(title, href) for title, href in links])
-            linksSoup = BeautifulSoup(linksHtml)
-            templateSoup = BeautifulSoup(templateHtml)
-
-            tag = templateSoup.find(class_='luwak-list')
-            tag.append(linksSoup)
-
-            if listTitle:
-                tag = templateSoup.find(class_='luwak-list-title')
-                tag.string.replace_with(listTitle)
-
-            return templateSoup.prettify()
-        elif type == 'index':
-            def a_formatter(title, href):
-                return "<a href='{1}'>{0}</a>".format(title, href)
-
-            templateHtml = self.get_template('index')
-            postListHtml = []
-            for title, href, article in pageInfo['postList']:
-
-                aHtml = '<a href=\'{1}\'>{0}</a>'.format(title.title(), href)
-                dateHtml = '<span>{}</span>'.format(article['created'].strftime("%b %d, %y"))
-                postStr = """
-                    <div class=\'index-post\'>
-                        <div class=\'post-title\'>{aHtml}</div>
-                        <div class=\'post-date\'>{dateHtml}</div>
-                    </div>
-                """
-                postListHtml.append(postStr.format(aHtml=aHtml,dateHtml=dateHtml))
-
-            listSoup = BeautifulSoup('<hr>'.join(postListHtml))    
-            templateSoup = BeautifulSoup(templateHtml)
-
-            tag = templateSoup.find(class_='luwak-index')
-            paginationTag = templateSoup.find(class_='luwak-pagination')
-
-            if not tag:
-                raise ValueError('No luwak-index in index template')
-            else:
-                tag.append(listSoup)
-
-
-            leftAdjacent = [a_formatter(title, href) for title, href in pageInfo['leftAdjacentPages']]
-            rightAdjacent = [a_formatter(title, href) for title, href in pageInfo['rightAdjacentPages']]
-
-            currentpage = a_formatter(pageInfo['currentPage'][0], pageInfo['currentPage'][1])
-
-            page_list = leftAdjacent + [currentpage] + rightAdjacent
-            for ind, val in enumerate(page_list):
-                if val == currentpage:
-                    cls = 'active'
-                else:
-                    cls = ''
-
-                page_list[ind] = "<li class='{}'>{}</li>".format(cls, val)
-
-
-            page_list = ['<ul class="pagination pull-right">'] + page_list + ['</ul>']
-
-            paginationSoup = BeautifulSoup(''.join(page_list))
-
-            tag = templateSoup.find(class_='luwak-pagination')
-
-            if paginationTag:
-                paginationTag.append(paginationSoup)
-            else:
-                print "WARNING: no page index"
-
-            return templateSoup.prettify()
+    pass
 
 class JinjaCombinator(CombinatorBase):
     def __init__(self, *args):
@@ -281,23 +283,17 @@ class TemplateCombinator(GenerationComponent):
 
         if 'template' not in self.settings:
             return os.path.join(defaultTemplatesDir, 'default')
-
         elif os.path.isdir(self.settings['template']):
             return self.settings['template']
         elif self.settings['template'] in foundTemplates:
             return os.path.join(localTemplatesDir, self.settings['template'])
         else:
-            raise ValueError("Couldn't find specified template")
+            raise ValueError("Couldn't find specified template: " + self.settings['template'])
 
     def combine(self, ctx, templateType=None):
         """Injects values into a html template.
 
         Use for standard type article i.e. with a title, content, tags, etc.
-
-        Attributes:
-            html: Html code to be inserted into the template.
-            fname: Filename of the source file. Defaults to None.
-            meta: Meta values of the source file. Defaults to an empty dict.
 
         Returns:
             str: html code with given values inserted.
@@ -308,12 +304,15 @@ class TemplateCombinator(GenerationComponent):
         try:
             with open(templateRcPath) as f:
                 templateRc = json.loads(f.read())
+        except ValueError:
+            print "Is rc file a valid JSON file?"
+            raise
         except IOError:
             print "Couldn't load template rc folder... Is it a valid template?"
             raise 
 
         if 'loader_type' not in templateRc:
-            logging.warning("template should have a type")
+            logging.error("template should have a type")
         elif templateRc['loader_type'] == 'default':
             pass
         elif templateRc['loader_type'] == 'jinja':
@@ -336,9 +335,6 @@ class TemplateCombinator(GenerationComponent):
         context = pageInfo
 
         return self.combine(context, templateType='index')
-
-    def combine_template(self, template, ctx):
-        pass
 
 if __name__ == '__main__':
     pass
